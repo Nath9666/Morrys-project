72364f5387b38230993ad9057f6718f5c63e45bf
Nathan <93925029+Nath9666@users.noreply.github.com>
2022-11-28 19:43:04 +0000

    login setup 1.26.18

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -7,13 +7,21 @@ const api_key = process.env.API_KEY;
 const api_secret = process.env.API_SECRET;
 const app_id = process.env.APP_ID;
 
-const signup = () => {
+const signup = async (req, res) => {
   try {
     const { fullName, username, password, phoneNumber } = req.body;
 
     const userId = crypto.randomBytes(16).toString("hex");
 
     const serverClient = connect(api_key, api_secret, app_id);
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const token = serverClient.createUserToken(userId);
+
+    res
+      .status(200)
+      .json({ token, fullName, username, userId, hashedPassword, phoneNumber });
   } catch (error) {
     console.log(error);
 
@@ -21,8 +29,14 @@ const signup = () => {
   }
 };
 
-const login = (req, res) => {
+const login = async (req, res) => {
   try {
+    const { username, password } = req.body;
+
+    const serverClient = connect(api_key, api_secret, app_id);
+    const client = StreamChat.getInstance(api_key, api_secret);
+
+    const { user } = await client.queryUsers({ name: username });
   } catch (error) {
     console.log(error);
 
