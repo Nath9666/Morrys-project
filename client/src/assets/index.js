054ab68f0a4c8f00add9b609bf404b6357125635
Nathan <93925029+Nath9666@users.noreply.github.com>
2022-11-28 17:55:25 +0000

    initialize auth and axios

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -6,3 +6,4 @@ export { LightningBolt } from "./LightningBolt";
 export { SearchIcon } from "./SearchIcon";
 export { default as LogoutIcon } from "./logout.png";
 export { default as HospitalIcon } from "./hospital.png";
+export { default as Sign } from "./signup.jpg";
---
+++ client/src/compenents/Auth.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import Cookies from "universal-cookie";
+import axios from "axios";
+
+import Sign from "../assets";
+
+const Auth = () => {
+  const [isSignup, setisSignup] = useState(false);
+
+  return (
+    <div className="auth__from-container">
+      <div className="auth__form-container_fields">
+        <div className="auth__form-container_fields-content">
+          <p>{isSignup ? "Sign Up" : "Sign In"}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Auth;
