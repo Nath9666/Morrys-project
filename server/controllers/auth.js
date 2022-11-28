766075a531f46688f6ab1f61e538dd3908989dc7
Nathan <93925029+Nath9666@users.noreply.github.com>
2022-11-28 19:25:50 +0000

    connect and signup  router

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,5 +1,33 @@
-const login = () => {};
+const { connect } = require("getstream");
+const bcrypt = require("bcrypt");
+const StreamChat = require("stream-chat");
+const crypto = require("crypto");
 
-const signup = () => {};
+const api_key = process.env.API_KEY;
+const api_secret = process.env.API_SECRET;
+const app_id = process.env.APP_ID;
+
+const signup = () => {
+  try {
+    const { fullName, username, password, phoneNumber } = req.body;
+
+    const userId = crypto.randomBytes(16).toString("hex");
+
+    const serverClient = connect(api_key, api_secret, app_id);
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({ message: error });
+  }
+};
+
+const login = (req, res) => {
+  try {
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({ message: error });
+  }
+};
 
 module.exports = { signup, login };
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -4,5 +4,7 @@ const { signup, login } = require("../controllers/auth");
 
 const router = express.Router();
 
-router.post("/signup");
-router.post("/login");
+router.post("/signup", signup);
+router.post("/login", login);
+
+module.exports = router;
