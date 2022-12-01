453f2416e615f86e42b8347427a52ca32dece376
Nathan <93925029+Nath9666@users.noreply.github.com>
2022-12-01 18:14:46 +0000

    add chat css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import Cookies from "universal-cookie";
 
 import { ChanelListContainer, ChannelConainer, Auth } from "./compenents";
 
+import "stream-chat-react/dist/css/index.css";
 import "./App.css";
 
 const cookies = new Cookies();
@@ -30,26 +31,27 @@ if (authToken) {
 
 function App() {
   const [createType, setCreateType] = useState("");
-  const [isCreating, setisCreating] = useState(false);
-  const [isEditing, setisEditing] = useState(false);
+  const [isCreating, setIsCreating] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
 
   if (!authToken) return <Auth />;
+  console.log(isEditing);
 
   return (
     <div className="app__wrapper">
       <Chat client={client} theme="team light">
         <ChanelListContainer
           isCreating={isCreating}
-          setisCreating={setisCreating}
+          setisCreating={setIsCreating}
           setCreateType={setCreateType}
-          setisEditing={setisEditing}
+          setisEditing={setIsEditing}
         />
 
         <ChannelConainer
           isCreating={isCreating}
-          setisCreating={setisCreating}
+          setIsCreating={setIsCreating}
           isEditing={isEditing}
-          setisEditing={setisEditing}
+          setIsEditing={setIsEditing}
           createType={createType}
         />
       </Chat>
