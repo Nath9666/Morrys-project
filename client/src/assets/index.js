48e1922e4ef2a1640b4a0ae333dbf9b32c20ab77
Nathan <93925029+Nath9666@users.noreply.github.com>
2022-12-05 02:19:04 +0000

    change logo

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -5,5 +5,5 @@ export { InviteIcon } from "./InviteIcon";
 export { LightningBolt } from "./LightningBolt";
 export { SearchIcon } from "./SearchIcon";
 export { default as LogoutIcon } from "./logout.png";
-export { default as HospitalIcon } from "./hospital.png";
+export { default as MorryIcon } from "./MorryIcon.png";
 export { default as SignIcon } from "./signup.jpg";
