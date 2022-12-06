c76c2860da96420e0d5c0cf23849b37346292b8a
Nath9666 <nathan.morel@efrei.net>
2022-12-06 11:54:28 +0000

    edit welcome page

--- client/src/compenents/Welcome.jsx
+++ client/src/compenents/Welcome.jsx
@@ -6,15 +6,16 @@ import { MorryIcon } from "../assets";
 import { Auth } from "./";
 
 const navigation = [
-  { name: "Product", href: "#" },
+  { name: "What is Morrys", href: "#", OnClick: "Home" },
   { name: "Features", href: "#" },
-  { name: "Marketplace", href: "#" },
-  { name: "Company", href: "#" },
+  { name: "Search", href: "#" },
+  { name: "About us", href: "#" },
 ];
 
 export default function Welcome() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
   const [log, setLog] = useState(false);
+  const [menu, setMenu] = useState("");
 
   if (log) return <Auth />;
 
@@ -35,6 +36,33 @@ export default function Welcome() {
           <defs>
             <linearGradient
               id="45de2b6b-92d5-4d68-a6a0-9b9b2abad533"
+              x1="1155.49"
+              x2="-78.208"
+              y1=".177"
+              y2="474.645"
+              gradientUnits="userSpaceOnUse"
+            >
+              <stop stopColor="#ffae00" />
+              <stop offset={1} stopColor="#364660" />
+            </linearGradient>
+          </defs>
+        </svg>
+      </div>
+      <div className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-2xl sm:top-[calc(100%-30rem)]">
+        <svg
+          className="relative left-[calc(50%+3rem)] h-[21.1875rem] max-w-none -translate-x-1/2 sm:left-[calc(50%+36rem)] sm:h-[42.375rem]"
+          viewBox="0 0 1155 678"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill="url(#ecb5b0c9-546c-4772-8c71-4d3f06d544bc)"
+            fillOpacity=".3"
+            d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z"
+          />
+          <defs>
+            <linearGradient
+              id="ecb5b0c9-546c-4772-8c71-4d3f06d544bc"
               x1="1155.49"
               x2="-78.208"
               y1=".177"
@@ -119,13 +147,13 @@ export default function Welcome() {
                 <div className="-my-6 divide-y divide-gray-500/10">
                   <div className="space-y-2 py-6">
                     {navigation.map((item) => (
-                      <a
+                      <p
                         key={item.name}
-                        href={item.href}
-                        className="-mx-3 block rounded-lg py-2 px-3 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-400/10"
+                        onClick={() => setMenu(item.OnClick)}
+                        className="-mx-3 block rounded-lg py-2 px-3 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-400/10 cursor-pointer"
                       >
                         {item.name}
-                      </a>
+                      </p>
                     ))}
                   </div>
                   <div className="py-6 cursor-pointer">
@@ -178,33 +206,6 @@ export default function Welcome() {
                     />
                   </div>
                 </div>
-              </div>
-              <div className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-2xl sm:top-[calc(100%-30rem)]">
-                <svg
-                  className="relative left-[calc(50%+3rem)] h-[21.1875rem] max-w-none -translate-x-1/2 sm:left-[calc(50%+36rem)] sm:h-[42.375rem]"
-                  viewBox="0 0 1155 678"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fill="url(#ecb5b0c9-546c-4772-8c71-4d3f06d544bc)"
-                    fillOpacity=".3"
-                    d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z"
-                  />
-                  <defs>
-                    <linearGradient
-                      id="ecb5b0c9-546c-4772-8c71-4d3f06d544bc"
-                      x1="1155.49"
-                      x2="-78.208"
-                      y1=".177"
-                      y2="474.645"
-                      gradientUnits="userSpaceOnUse"
-                    >
-                      <stop stopColor="#ffae00" />
-                      <stop offset={1} stopColor="#364660" />
-                    </linearGradient>
-                  </defs>
-                </svg>
               </div>
             </div>
           </div>
