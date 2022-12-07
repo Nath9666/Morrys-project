33c5388c790c02fa9231d909d901eb36a4231da8
Ethan <ethansuissa@gmail.com>
2022-12-08 00:27:03 +0100

    Merge avec server side : adaptation de App.jsx pour 2 personnes travaillant séparément

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,15 +4,17 @@ import { ChannelList, Chat } from "stream-chat-react";
 import Cookies from "universal-cookie";
 
 import {
-  ChanelListContainer,
-  ChannelConainer,
-  Auth,
-  Welcome,
+    ChanelListContainer,
+    ChannelConainer,
+    Auth,
+    Welcome,
+    NewProject,
 } from "./compenents";
 
 import "stream-chat-react/dist/css/index.css";
 import "./App.css";
 import "./index.css";
+import "./Project.css";
 
 const cookies = new Cookies();
 
@@ -36,34 +38,47 @@ if (authToken) {
 }
 
 function App() {
-  const [createType, setCreateType] = useState("");
-  const [isCreating, setIsCreating] = useState(false);
-  const [isEditing, setIsEditing] = useState(false);
 
-  if (!authToken) return <Welcome />;
+    const [createType, setCreateType] = useState("");
+    const [isCreating, setIsCreating] = useState(false);
+    const [isEditing, setIsEditing] = useState(false);
 
-  <Auth />;
+    let Codeur = "Ethan"
+    switch(Codeur){
+        case "Ethan": {
+            return (
+                <>
+                    <NewProject/>
+                </>
 
-  return (
-    <div className="app__wrapper">
-      <Chat client={client} theme="team light">
-        <ChanelListContainer
-          isCreating={isCreating}
-          setIsCreating={setIsCreating}
-          setCreateType={setCreateType}
-          setIsEditing={setIsEditing}
-        />
+            );
+        }
+        case "Nathan":{
+            if (!authToken) return <Welcome />;
+            <Auth />;
+            return(
+                <div className="app__wrapper">
+                    <Chat client={client} theme="team light">
+                        <ChanelListContainer
+                            isCreating={isCreating}
+                            setIsCreating={setIsCreating}
+                            setCreateType={setCreateType}
+                            setIsEditing={setIsEditing}
+                        />
 
-        <ChannelConainer
-          isCreating={isCreating}
-          setIsCreating={setIsCreating}
-          isEditing={isEditing}
-          setIsEditing={setIsEditing}
-          createType={createType}
-        />
-      </Chat>
-    </div>
-  );
+                        <ChannelConainer
+                            isCreating={isCreating}
+                            setIsCreating={setIsCreating}
+                            isEditing={isEditing}
+                            setIsEditing={setIsEditing}
+                            createType={createType}
+                        />
+                    </Chat>
+                </div>
+            );
+        }
+        default: document.write("Incorrect codeur");
+    }
 }
 
 export default App;
