3fad80ee47a4adc0cd1b8a4a4a6a08e93bc89e3f
Nathan <93925029+Nath9666@users.noreply.github.com>
2022-12-12 16:50:13 +0000

    init data base

--- server/serv.js
+++ server/serv.js
@@ -1,13 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-const authRoutes = require("./routes/auth.js");
-
 const app = express();
 const PORT = process.env.PORT || 3001;
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
